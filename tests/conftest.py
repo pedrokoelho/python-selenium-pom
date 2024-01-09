88ec9a088c87d5c891c01c5fd6dc827abd6a73ac
pedrocoelho.tester <86770446+pedrokoelho@users.noreply.github.com>
2024-01-09 22:11:45 +0000

    Improved conftest to run the tests on firefox and edge

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,20 @@ from selenium import webdriver
 @pytest.fixture()
 def driver():
     print("\nAccessing driver fixture on conftest -> Creating chrome driver")
-    # open browser
-    my_driver = webdriver.Chrome()
+    
+    # open Chrome browser
+    #my_driver = webdriver.Chrome()
+    
+    # open Firefox browser
+    #my_driver = webdriver.Firefox()
+
+    # open Edge browser
+    # with option - maximize window
+    options = webdriver.EdgeOptions()
+    options.add_argument("--start-maximized")
+    my_driver = webdriver.Edge(options=options)
+    
     yield my_driver
+    
     print("\nClosing chrome driver")
     my_driver.quit()
