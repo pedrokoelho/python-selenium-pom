ea33a24260726ed8cca52e169e564ecb7db31f82
pedrocoelho.tester <86770446+pedrokoelho@users.noreply.github.com>
2024-01-09 22:39:50 +0000

    Add method in conftest to add command line option to select the browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,36 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def driver():
-    print("\nAccessing driver fixture on conftest -> Creating chrome driver")
-    
-    # open Chrome browser
-    #my_driver = webdriver.Chrome()
-    
-    # open Firefox browser
-    #my_driver = webdriver.Firefox()
+def driver(request):
 
-    # open Edge browser
-    # with option - maximize window
-    options = webdriver.EdgeOptions()
-    options.add_argument("--start-maximized")
-    my_driver = webdriver.Edge(options=options)
+    # get the option selected in the command line
+    browser = request.config.getoption("--browser")
+
+    print(f"\nCreating {browser} driver")
+    
+    if browser == "chrome":
+        # open Chrome browser
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        # open Firefox browser
+        my_driver = webdriver.Firefox()
+    elif browser == "edge":
+        # open Edge browser
+        # with option - maximize window
+        options = webdriver.EdgeOptions()
+        options.add_argument("--start-maximized")
+        my_driver = webdriver.Edge(options=options)
+    else:
+        raise TypeError("Expected 'chrome', 'edge' or firefox but got {browser}")
     
     yield my_driver
     
-    print("\nClosing chrome driver")
+    print(f"\nClosing {browser} driver")
     my_driver.quit()
+
+
+# Method to add command line option
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser to run the tests: Chrome | Edge | Firefox"
+    )
