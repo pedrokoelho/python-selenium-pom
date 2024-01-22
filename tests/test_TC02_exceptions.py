790f21eb5cbc6894a13346c934075eaeebf64d93
pedrocoelho.tester <86770446+pedrokoelho@users.noreply.github.com>
2024-01-22 20:37:47 +0000

    Added test exception element not interactable

--- tests/test_TC02_exceptions.py
+++ tests/test_TC02_exceptions.py
@@ -29,3 +29,26 @@ class TestException:
         #input_row_2_locator = driver.find_element(By.XPATH, '//div[@id="row2"]//following::input')
         
         assert input_row_2_element.is_displayed(), 'Row 2 is not displayed'
+
+    @pytest.mark.exceptions
+    @pytest.mark.debug
+    def test_TC02_2_element_not_interactable_exception(self, driver):
+
+        # Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        # click Add btn
+        btn_add = driver.find_element(By.XPATH, '//button[@id="add_btn"]')
+        btn_add.click()
+
+        # EXPLICIT WAIT
+        # create a new instance of the WebDriverWait
+        wait = WebDriverWait(driver, 10)
+        input_row_2 = wait.until(ec.presence_of_element_located((By.XPATH, '//div[@id="row2"]//following::input')))
+
+        # Type text into the second input field
+        input_row_2.send_keys('Teste')
+
+        # Click Save button using locator By.name(“Save”)
+        btn_save = driver.find_element(By.NAME, "Save")
+        btn_save.click()
